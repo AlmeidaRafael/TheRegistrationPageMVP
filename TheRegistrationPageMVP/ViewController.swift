8ac4dbea0c68a6bab8e6718a6e9780a6f1f9672a
Rafael Almeida <rafinhascott@hotmail.com>
2018-11-14 15:52:32 -0200

    Initial Commit

---
+++ TheRegistrationPageMVP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheRegistrationPageMVP
+//
+//  Created by Rafael Almeida Oliveira on 14/11/18.
+//  Copyright © 2018 Rafael Almeida Oliveira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
